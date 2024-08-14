0645a59e3eceabc909c511479b114a87f7d0eee1
Itay Sadras <itaysadr@gmail.com>
2024-08-14 18:29:46 +0300

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.scss'
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <div className='form-container'>
+
+        </div>
+        <div className='carousel-container'>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
