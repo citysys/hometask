e625388abf8d082a4370dc8d39f5a031ff449522
MeirJC <meir84@gmail.com>
2025-03-11 10:48:22 +0200

    base page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="main-layout">
+      <header className="main-layout_header">
+        <h1>
+          רישוי עסקים מהמשרד ומכל מקום <span>אפליקציית שטח למפקח</span>
+        </h1>
+      </header>
+      <section className="main-layout_content">
+        <h1>Content and Form</h1>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
